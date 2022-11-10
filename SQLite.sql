8063182a716e55987c97d83ad9f62a0e172fbc41
Erkinbek008 <amiroverkinbek008@gmail.com>
2022-11-10 16:22:26 +0500

    Erkinbek added SQLite.sql file

---
+++ SQLite.sql
@@ -0,0 +1,57 @@
+INSERT INTO school VALUES(
+  '1',
+  'Xumoyun',
+  '5',
+  'TATU'
+);
+
+INSERT INTO school VALUES(
+  '2',
+  'Abdumalik',
+  '3',
+  'Oxford'
+);
+INSERT INTO school VALUES(
+  '3',
+  'Jo`rabek',
+  '4',
+  'Iqtisodiyot universiteti'
+);
+INSERT INTO school VALUES(
+  '4',
+  'Sobir',
+  '4',
+  'Myama university'
+);INSERT INTO school VALUES(
+  '5',
+  'Qodir',
+  '5',
+  'Stamatology university'
+);INSERT INTO school VALUES(
+  '6',
+  'Sarvinoz',
+  '4',
+  'Pedagokika universiteti'
+);INSERT INTO school VALUES(
+  '7',
+  'Gulnoza',
+  '5',
+  'TATU'
+);INSERT INTO school VALUES(
+  '8',
+  'Murod',
+  '3',
+  'Garvard'
+);INSERT INTO school VALUES(
+  '9',
+  'Jamila',
+  '5',
+  'Yuridistika universiteti'
+);INSERT INTO school VALUES(
+  '10',
+  'Mirafzal',
+  '5',
+  'TATU'
+);
+SELECT * from school;
+
